eaf2a231659ea94764e671229253a8ab7981c2bd
Jackcheal Dang <jackchealdang@gmail.com>
2025-03-26 19:39:43 -0500

    fix: fix mobile kb

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -452,12 +452,12 @@ export function Calculator() {
                         handleUpdatePersonName(person.id, e.target.value)
                       }
                       className="p-0 w-44 border-none outline-none shadow-none focus-visible:ring-0 underline"
-                      // onFocus={(e) => {
-                      //   e.currentTarget.setSelectionRange(
-                      //     e.currentTarget.value.length,
-                      //     e.currentTarget.value.length,
-                      //   );
-                      // }}
+                      onFocus={(e) => {
+                        e.currentTarget.setSelectionRange(
+                          0,
+                          e.currentTarget.value.length,
+                        );
+                      }}
                     />
                   </BlurFade>
                 </div>
@@ -507,12 +507,12 @@ export function Calculator() {
                           handleUpdateItemName(item.id, e.target.value)
                         }
                         className="p-0 w-44 border-none outline-none shadow-none focus-visible:ring-0 underline"
-                        // onFocus={(e) => {
-                        //   e.currentTarget.setSelectionRange(
-                        //     e.currentTarget.value.length,
-                        //     e.currentTarget.value.length,
-                        //   );
-                        // }}
+                        onFocus={(e) => {
+                          e.currentTarget.setSelectionRange(
+                            0,
+                            e.currentTarget.value.length,
+                          );
+                        }}
                       />
                     </BlurFade>
                   </div>
