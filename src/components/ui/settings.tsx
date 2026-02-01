894a575db89abec3bff3cc575e5ca23ceeedb0e9
Jackcheal Dang <jackchealdang@gmail.com>
2026-02-01 04:41:16 -0600

    use checked prop to set state

--- src/components/ui/settings.tsx
+++ src/components/ui/settings.tsx
@@ -1,28 +1,68 @@
-import { Switch } from "@/components/ui/switch";
-import { Button } from "./button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "./dropdown-menu";
-import React from "react";
+import { Switch } from '@/components/ui/switch';
+import { Button } from './button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './dropdown-menu';
+import React from 'react';
 
-export function Settings({tipEvenly, setTipEvenly, taxEvenly, setTaxEvenly}: {tipEvenly: boolean, setTipEvenly: (value: boolean) => void, taxEvenly: boolean, setTaxEvenly: (value: boolean) => void}) {
+export function Settings({
+  tipEvenly,
+  setTipEvenly,
+  taxEvenly,
+  setTaxEvenly,
+}: {
+  tipEvenly: boolean;
+  setTipEvenly: (value: boolean) => void;
+  taxEvenly: boolean;
+  setTaxEvenly: (value: boolean) => void;
+}) {
   const [dropdownOpen, setDropdownOpen] = React.useState(false);
 
   return (
-    <DropdownMenu open={dropdownOpen} onOpenChange={(val) => setDropdownOpen(val)}>
-        <DropdownMenuTrigger asChild onClick={() => { setDropdownOpen((val) => !val); }}>
-        <Button className="cursor-pointer" variant="outline">Settings</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="z-50" sideOffset={6}>
+    <DropdownMenu
+      open={dropdownOpen}
+      onOpenChange={(val) => setDropdownOpen(val)}
+    >
+      <DropdownMenuTrigger
+        asChild
+        onClick={() => {
+          setDropdownOpen((val) => !val);
+        }}
+      >
+        <Button
+          className='cursor-pointer'
+          variant='outline'
+        >
+          Settings
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='z-50'
+        sideOffset={6}
+      >
         <DropdownMenuGroup>
-            <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+          <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
             Split Tip Evenly
-            <Switch className="cursor-pointer" onClick={() => setTipEvenly(!tipEvenly)}/>
-            </DropdownMenuItem>
-            <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+            <Switch
+              className='cursor-pointer'
+              onClick={() => setTipEvenly(!tipEvenly)}
+              checked={tipEvenly}
+            />
+          </DropdownMenuItem>
+          <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
             Split Tax Evenly
-            <Switch className="cursor-pointer" onClick={() => setTaxEvenly(!taxEvenly)}/>
-            </DropdownMenuItem>
+            <Switch
+              className='cursor-pointer'
+              onClick={() => setTaxEvenly(!taxEvenly)}
+              checked={taxEvenly}
+            />
+          </DropdownMenuItem>
         </DropdownMenuGroup>
-        </DropdownMenuContent>
+      </DropdownMenuContent>
     </DropdownMenu>
-  )
+  );
 }
